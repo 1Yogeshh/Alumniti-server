f324cc95a38f5e95ff7e5643340f1dedf204cd99
Riya Mehta <riyamehta1608@gmail.com>
2025-02-22 17:43:02 +0530

    socket setup

---
+++ socket.js
@@ -0,0 +1,30 @@
+const { Server } = require("socket.io");
+
+let io;
+
+const initializeSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: ["https://alumniti-app.vercel.app", "http://localhost:3000"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+
+    socket.on("sendMessage", (data) => {
+      io.emit("receiveMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
+
+const getSocketInstance = () => io;
+
+module.exports = { initializeSocket, getSocketInstance };
